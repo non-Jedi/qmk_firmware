ea9bea4f8f6c3851703978d5286e2587ee6d6655
Jeremy Bernhardt <bernhardtjeremy@gmail.com>
2020-02-28 09:15:09 -0600

    added via for faunch

--- keyboards/gboards/k/faunchpad/rules.mk
+++ keyboards/gboards/k/faunchpad/rules.mk
@@ -10,9 +10,6 @@ CONSOLE_ENABLE 		= yes   # Console for debug(+400)
 COMMAND_ENABLE 		= no		# Commands for debug and configuration
 NKRO_ENABLE 			= yes   # USB Nkey Rollover
 
-# Engine defs
-VPATH               +=  keyboards/gboards/ 
-SRC                 +=  g/engine.c 
 
 OPT_DEFS 						+= -DDEBUG_MATRIX
 EXTRAFLAGS 					+= -flto
